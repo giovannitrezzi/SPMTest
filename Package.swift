c938856366192a53818f40690359c23a4087e52a
giovannitrezzi <148251715+giovannitrezzi@users.noreply.github.com>
2023-12-01 11:48:03 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,23 +24,22 @@ let package = Package(
 //            dependencies: ["Alamofire", "Factory", .product(name: "SwiftJWT", package: "Swift-JWT"), "ZIPFoundation"],
 //            path: "Sources/ENMobileUtils"
 //        ),
-        .target(
+        .binaryTarget(
             name: "ENMobileUtils",
+            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileUtils/1.0.3/ENMobileUtils.zip",
+            checksum: "e450e0daa4d37cac24fbc18881334a2b6f506217f8cd867fc8b2a7c113649fdf"
+            //checksum: "b3b80618364cce2589fc847462a661cd429bfce5e5588f01e6329be311ab7f0b"
+        ),
+        .target(
+            name: "ENMobileUtilsDependencies",
             dependencies: [
-                .target(name: "ENMobileUtilsFramework"),
+                .target(name: "ENMobileUtils"),
                 .product(name: "Alamofire", package: "Alamofire"),
                 .product(name: "SwiftJWT", package: "Swift-JWT"),
                 .product(name: "Factory", package: "Factory"),
                 .product(name: "ZIPFoundation", package: "ZIPFoundation")
             ],
             path: "Wrapper"
-        ),
-        .binaryTarget(
-            name: "ENMobileUtilsFramework",
-            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileUtils/1.0.3/ENMobileUtils.zip",
-            checksum: "e450e0daa4d37cac24fbc18881334a2b6f506217f8cd867fc8b2a7c113649fdf"
-            //checksum: "b3b80618364cce2589fc847462a661cd429bfce5e5588f01e6329be311ab7f0b"
         )
-        
     ]
 )
