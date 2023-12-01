ccb84cfba8f74d44b4b291c34171423ea0542c76
giovannitrezzi <148251715+giovannitrezzi@users.noreply.github.com>
2023-12-01 10:41:47 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "ENMobileUtils",
+            name: "ENMobileUtilsFramework",
             targets: ["ENMobileUtils"]
         )
     ],
