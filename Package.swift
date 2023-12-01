a08b667562b66068dbb3b0038d4d6436083f0efb
giovannitrezzi <148251715+giovannitrezzi@users.noreply.github.com>
2023-12-01 15:18:20 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         .binaryTarget(
             name: "ENMobileUtils",
             url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileUtils/1.0.3/ENMobileUtils.zip",
-            checksum: "e450e0daa4d37cac24fbc18881334a2b6f506217f8cd867fc8b2a7c113649fdf"
+            checksum: "5a8b9581f346b73714f05a4bbe950276202d80429186f2fa24bb76976736af79"
             //checksum: "b3b80618364cce2589fc847462a661cd429bfce5e5588f01e6329be311ab7f0b"
         )
     ]
