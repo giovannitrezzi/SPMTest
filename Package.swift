9c33b2738e0a82431ec88fd979e8513a5eeb80c7
giovannitrezzi <148251715+giovannitrezzi@users.noreply.github.com>
2023-12-01 17:27:46 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,8 +25,9 @@ let package = Package(
 //            path: "Sources/ENMobileUtils"
 //        ),
         .target(
-            name: "ENMobileUtilsDependencies",
+            name: "ENMobileUtils",
             dependencies: [
+                .target(name: "ENMobileUtilsFramework"),
                 .product(name: "Alamofire", package: "Alamofire"),
                 .product(name: "SwiftJWT", package: "Swift-JWT"),
                 .product(name: "Factory", package: "Factory"),
@@ -35,7 +36,7 @@ let package = Package(
             path: "Wrapper"
         ),
         .binaryTarget(
-            name: "ENMobileUtils",
+            name: "ENMobileUtilsFramework",
             url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileUtils/1.0.3/ENMobileUtils.zip",
             checksum: "5a8b9581f346b73714f05a4bbe950276202d80429186f2fa24bb76976736af79"
             //checksum: "b3b80618364cce2589fc847462a661cd429bfce5e5588f01e6329be311ab7f0b"
