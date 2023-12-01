177613b06723d0ffcdb6e9d956258cde1fe314e8
giovannitrezzi <148251715+giovannitrezzi@users.noreply.github.com>
2023-12-01 12:41:45 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "ENMobileUtils",
+            type: .dynamic,
             targets: ["ENMobileUtils", "ENMobileUtilsDependencies"]
         )
     ],
