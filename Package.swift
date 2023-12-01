b0c358d5aaa51cf30a855efa2053632899f69677
giovannitrezzi <148251715+giovannitrezzi@users.noreply.github.com>
2023-12-01 10:54:56 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,19 +26,26 @@ let package = Package(
 //        ),
         .binaryTarget(
             name: "ENMobileUtils",
+            dependencies: [
+                .target(name: "ENMobileUtils"),
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "SwiftJWT", package: "Swift-JWT"),
+                .product(name: "Factory", package: "Factory"),
+                .product(name: "ZIPFoundation", package: "ZIPFoundation"),
+            ],
             url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileUtils/1.0.3/ENMobileUtils.zip",
             checksum: "e450e0daa4d37cac24fbc18881334a2b6f506217f8cd867fc8b2a7c113649fdf"
             //checksum: "b3b80618364cce2589fc847462a661cd429bfce5e5588f01e6329be311ab7f0b"
         ),
         // .target(
         //     name: "ENMobileUtilsFramework",
-        //     dependencies: [
-        //         .target(name: "ENMobileUtils"),
-        //         .product(name: "Alamofire", package: "Alamofire"),
-        //         .product(name: "SwiftJWT", package: "Swift-JWT"),
-        //         .product(name: "Factory", package: "Factory"),
-        //         .product(name: "ZIPFoundation", package: "ZIPFoundation"),
-        //     ],
+            // dependencies: [
+            //     .target(name: "ENMobileUtils"),
+            //     .product(name: "Alamofire", package: "Alamofire"),
+            //     .product(name: "SwiftJWT", package: "Swift-JWT"),
+            //     .product(name: "Factory", package: "Factory"),
+            //     .product(name: "ZIPFoundation", package: "ZIPFoundation"),
+            // ],
         //     path: "ENMobileUtils.xcframeworks"
         // )
     ]
