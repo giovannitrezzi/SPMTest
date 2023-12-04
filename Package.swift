1a3616fc94639df85ef99ba5230e07af4ad3892f
giovannitrezzi <148251715+giovannitrezzi@users.noreply.github.com>
2023-12-04 12:02:29 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "ENMobileUtils",
-            targets: ["ENMobileUtilsTarget"]
+            targets: ["ENMobileUtilsWrapper"]
         ),
     ],
     dependencies: [
